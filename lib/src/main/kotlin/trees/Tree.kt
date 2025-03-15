0fda5b9050ea52aabbeda6835164497c6814115c
Semyon <semen.rosin1@gmail.com>
2025-03-15 17:48:53 +0300

    Build gradle project. Add abstract classes node and tree with iterator realization

---
+++ lib/src/main/kotlin/trees/Tree.kt
@@ -0,0 +1,49 @@
+package trees
+
+import nodes.Node
+
+abstract class Tree<K : Comparable<K>, V, treeNode : Node<K, V, treeNode>> : Iterable<treeNode> {
+    internal var size: Int = 0
+    protected var root: treeNode? = null
+
+    abstract fun insert(key: K, newValue: V)
+
+    abstract fun remove(key: K)
+
+    abstract fun find(key: K): V?
+
+    override fun iterator(): Iterator<treeNode> {
+        return object : Iterator<treeNode> {
+            var wasRootInQueue = false
+            val rootCopy = root
+            val queue: ArrayDeque<treeNode> = ArrayDeque()
+            override fun hasNext(): Boolean {
+                if (!wasRootInQueue) {
+                    if (rootCopy != null) {
+                        queue.add(rootCopy)
+                        wasRootInQueue = true
+                        bfs(rootCopy)
+                    } else return false
+                }
+                return queue.isNotEmpty()
+            }
+
+            override fun next(): treeNode {
+                return queue.removeFirst()
+            }
+
+            fun bfs(node: treeNode) {
+                val leftChild = node.leftChild
+                val rightChild = node.rightChild
+                if (leftChild != null) {
+                    queue.add(leftChild)
+                    bfs(leftChild)
+                }
+                if (rightChild != null) {
+                    queue.add(rightChild)
+                    bfs(rightChild)
+                }
+            }
+        }
+    }
+}
