9e15e428a6a9ca247fc64dcbf6bac8da0ccc881f
Semyon <semen.rosin1@gmail.com>
2025-03-17 21:55:04 +0300

    Add remove method. Fix insert

--- lib/src/main/kotlin/trees/BSTree.kt
+++ lib/src/main/kotlin/trees/BSTree.kt
@@ -12,43 +12,100 @@ class BSTree<K : Comparable<K>, V> : Tree<K, V, BSNode<K, V>>() {
             var currNode = root ?: return
             var isAncFound = false
             val newNode = BSNode(key, newValue)
+
             while (!isAncFound) {
                 if (key < currNode.key) {
                     if (currNode.leftChild == null) {
                         currNode.leftChild = newNode
                         isAncFound = true
-                    } else currNode = currNode.leftChild ?: (root ?: return)
+                    } else currNode = currNode.leftChild ?: return
                 } else if (key > currNode.key) {
                     if (currNode.rightChild == null) {
                         currNode.rightChild = newNode
                         isAncFound = true
-                    } else currNode = currNode.rightChild ?: (root ?: return)
+                    } else currNode = currNode.rightChild ?: return
                 } else {
                     currNode.value = newValue
+                    isAncFound = true
                 }
             }
         }
     }
 
-    override fun remove(key: K) {}
-
-    override fun find(key: K): V? {
-        if (size == 0) return null
-        var currNode = root
+    override fun remove(key: K) {
+        var removingNode = root ?: return
         var isFound = false
+        var parent = root ?: return
+
         while (!isFound) {
-            if (currNode != null) {
-                if (key > currNode.key) {
-                    if (currNode.rightChild == null) return null
-                    else currNode = currNode.rightChild ?: root
-                } else if (key < currNode.key) {
-                    if (currNode.leftChild == null) return null
-                    else currNode = currNode.leftChild ?: root
-                } else {
-                    isFound = true
-                }
+            if (key > removingNode.key) {
+                parent = removingNode
+                removingNode = removingNode.rightChild ?: return
+            } else if (key < removingNode.key) {
+                parent = removingNode
+                removingNode = removingNode.leftChild ?: return
+            } else {
+                isFound = true
             }
         }
-        return currNode?.value
+
+        var isLeft = false
+        if (parent.key > key) isLeft = true
+
+        if (removingNode.leftChild == null && removingNode.rightChild == null) {
+            if (isLeft) parent.leftChild = null
+            else parent.rightChild = null
+        } else if (removingNode.leftChild != null && removingNode.rightChild == null) {
+            if (isLeft) parent.leftChild = removingNode.leftChild
+            else parent.rightChild = removingNode.leftChild
+        } else if (removingNode.rightChild != null && removingNode.leftChild == null) {
+            if (isLeft) parent.leftChild = removingNode.rightChild
+            else parent.rightChild = removingNode.rightChild
+        } else {
+            if (removingNode.rightChild?.leftChild == null) {
+                removingNode.rightChild?.leftChild = removingNode.leftChild
+                if (key == root?.key) {
+                    root = removingNode.rightChild
+                } else {
+                    parent.rightChild = removingNode
+                }
+                size--
+                return
+            }
+
+            var replacement = removingNode.rightChild ?: return
+            var rpParent = removingNode
+
+            while (replacement.leftChild != null) {
+                rpParent = replacement
+                replacement = replacement.leftChild ?: return
+            }
+
+            rpParent.leftChild = replacement.rightChild
+            replacement.leftChild = removingNode.leftChild
+            replacement.rightChild = removingNode.rightChild
+
+            if (isLeft) parent.leftChild = replacement
+            else parent.rightChild = replacement
+        }
+        size--
+        return
+    }
+
+    override fun find(key: K): V? {
+        var currNode = root ?: return null
+        var isFound = false
+
+        while (!isFound) {
+            if (key > currNode.key) {
+                currNode = currNode.rightChild ?: return null
+            } else if (key < currNode.key) {
+                currNode = currNode.leftChild ?: return null
+            } else {
+                isFound = true
+            }
+        }
+
+        return currNode.value
     }
 }
