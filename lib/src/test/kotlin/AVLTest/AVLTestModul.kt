08ca6f69fe30f9777d44c0a78f0aae941a25aaa0
dmitriimotuzov <dimamotuzov07@gmail.com>
2025-03-20 02:10:44 +0300

    write all modul test for insert

--- lib/src/test/kotlin/AVLTest/AVLTestModul.kt
+++ lib/src/test/kotlin/AVLTest/AVLTestModul.kt
@@ -77,32 +77,176 @@ class AVLTestModul {
 
     @Test
     fun test_simple_insert() {
+        val tree = AVLTree<Int,Int>()
+        tree.insert(10,10)
+        tree.insert(20,20)
+        tree.insert(5,5)
+        tree.insert(15,15)
+        tree.insert(25,25)
 
+
+        val correctTree = ArrayDeque<CorrectNode>()
+        correctTree.add(CorrectNode(10,10,5,5,20,20))
+        correctTree.add(CorrectNode(5,5,null,null,null,null))
+        correctTree.add(CorrectNode(20,20,15,15,25,25))
+        correctTree.add(CorrectNode(15,15,null,null,null,null))
+        correctTree.add(CorrectNode(25,25,null,null,null,null))
+
+
+        assertEquals(equalsTree(tree,correctTree),true)
     }
 
     @Test
     fun test_left_rotation_insert() {
+        val tree = AVLTree<Int,Int>()
+        tree.insert(50,10)
+        tree.insert(51,20)
+        tree.insert(52,5)
+        tree.insert(53,15)
+        tree.insert(54,25)
 
+
+        val correctTree = ArrayDeque<CorrectNode>()
+        correctTree.add(CorrectNode(51,20,50,10,53,15))
+        correctTree.add(CorrectNode(50,10,null,null,null,null))
+        correctTree.add(CorrectNode(53,15,52,5,54,25))
+        correctTree.add(CorrectNode(52,5,null,null,null,null))
+        correctTree.add(CorrectNode(54,25,null,null,null,null))
+
+
+        assertEquals(equalsTree(tree,correctTree),true)
     }
 
     @Test
     fun test_right_rotation_insert() {
+        val tree = AVLTree<Int,Int>()
+        tree.insert(58,10)
+        tree.insert(11,20)
+        tree.insert(4,5)
+        tree.insert(3,15)
+        tree.insert(1,25)
 
+
+        val correctTree = ArrayDeque<CorrectNode>()
+        correctTree.add(CorrectNode(11,20,3,15,58,10))
+        correctTree.add(CorrectNode(3,15,1,25,4,5))
+        correctTree.add(CorrectNode(1,25,null,null,null,null))
+        correctTree.add(CorrectNode(4,5,null,null,null,null))
+        correctTree.add(CorrectNode(58,10,null,null,null,null))
+
+
+        assertEquals(equalsTree(tree,correctTree),true)
     }
 
     @Test
     fun test_big_left_rotation_insert() {
+        val tree = AVLTree<Int,Int>()
+        tree.insert(10,10)
+        tree.insert(20,20)
+        tree.insert(30,30)
+        tree.insert(40,40)
+        tree.insert(50,50)
+        tree.insert(25,25)
 
+
+        val correctTree = ArrayDeque<CorrectNode>()
+        correctTree.add(CorrectNode(30,30,20,20,40,40))
+        correctTree.add(CorrectNode(20,20,10,10,25,25))
+        correctTree.add(CorrectNode(10,10,null,null,null,null))
+        correctTree.add(CorrectNode(25,25,null,null,null,null))
+        correctTree.add(CorrectNode(40,40,null,null,50,50))
+        correctTree.add(CorrectNode(50,50,null,null,null,null))
+
+
+        assertEquals(equalsTree(tree,correctTree),true)
     }
 
     @Test
     fun test_big_right_rotation_insert() {
+        val tree = AVLTree<Int,Int>()
+        tree.insert(50,50)
+        tree.insert(40,40)
+        tree.insert(30,30)
+        tree.insert(20,20)
+        tree.insert(10,10)
+        tree.insert(35,35)
 
+
+        val correctTree = ArrayDeque<CorrectNode>()
+        correctTree.add(CorrectNode(30,30,20,20,40,40))
+        correctTree.add(CorrectNode(20,20,10,10,null,null))
+        correctTree.add(CorrectNode(10,10,null,null,null,null))
+        correctTree.add(CorrectNode(40,40,35,35,50,50))
+        correctTree.add(CorrectNode(35,35,null,null,null,null))
+        correctTree.add(CorrectNode(50,50,null,null,null,null))
+
+
+        assertEquals(equalsTree(tree,correctTree),true)
     }
 
     @Test
     fun test_all_ot_off_rotation_insert() {
+        val tree = AVLTree<Int,Int>()
+        tree.insert(87,87)
+        tree.insert(31,31)
+        tree.insert(42,42)
+        tree.insert(64,64)
+        tree.insert(73,73)
+        tree.insert(95,95)
+        tree.insert(24,24)
+        tree.insert(29,29)
+        tree.insert(5,5)
+        tree.insert(78,78)
+        tree.insert(58,58)
+        tree.insert(28,28)
+        tree.insert(43,43)
+        tree.insert(3,3)
+        tree.insert(57,57)
 
+
+        val correctTree = ArrayDeque<CorrectNode>()
+        correctTree.add(CorrectNode(42,42,24,24,73,73))
+        correctTree.add(CorrectNode(24,24,5,5,29,29))
+        correctTree.add(CorrectNode(5,5,3,3,null,null))
+        correctTree.add(CorrectNode(3,3,null,null,null,null))
+        correctTree.add(CorrectNode(29,29,28,28,31,31))
+        correctTree.add(CorrectNode(28,28,null,null,null,null))
+        correctTree.add(CorrectNode(31,31,null,null,null,null))
+        correctTree.add(CorrectNode(73,73,58,58,87,87))
+        correctTree.add(CorrectNode(58,58,43,43,64,64))
+        correctTree.add(CorrectNode(43,43,null,null,57,57))
+        correctTree.add(CorrectNode(57,57,null,null,null,null))
+        correctTree.add(CorrectNode(64,64,null,null,null,null))
+        correctTree.add(CorrectNode(87,87,78,78,95,95))
+        correctTree.add(CorrectNode(78,78,null,null,null,null))
+        correctTree.add(CorrectNode(95,95,null,null,null,null))
+
+
+        assertEquals(equalsTree(tree,correctTree),true)
+    }
+
+    @Test
+    fun test_insert_new_value_in_key(){
+        val tree = AVLTree<Int,Int>()
+        tree.insert(50,50)
+        tree.insert(40,40)
+        tree.insert(30,30)
+        tree.insert(20,20)
+        tree.insert(10,10)
+        tree.insert(35,35)
+        tree.insert(40,100)
+
+
+        val correctTree = ArrayDeque<CorrectNode>()
+        correctTree.add(CorrectNode(30,30,20,20,40,100))
+        correctTree.add(CorrectNode(20,20,10,10,null,null))
+        correctTree.add(CorrectNode(10,10,null,null,null,null))
+        correctTree.add(CorrectNode(40,100,35,35,50,50))
+        correctTree.add(CorrectNode(35,35,null,null,null,null))
+        correctTree.add(CorrectNode(50,50,null,null,null,null))
+
+
+        assertEquals(equalsTree(tree,correctTree),true)
     }
 
     @Test
@@ -142,8 +286,6 @@ class AVLTestModul {
 
     @Test
     fun test_remove_root() {
-
-
         val tree = AVLTree<Int, Int>()
         tree.insert(5,5)
         tree.insert(6,4)
